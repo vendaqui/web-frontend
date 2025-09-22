e3747f40d031ddf80363ea56e3b13dce7dfc438d
VictorRibeiroH <contato.victoribeiro@gmail.com>
2025-09-22 10:19:56 -0300

    chore(ci): relax commitlint rules for semantic-release

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	ignores: [(message) => message.startsWith('chore(release):')],
+	rules: {
+		'body-max-line-length': [0, 'always'],
+		'footer-max-line-length': [0, 'always']
+	}
+};
