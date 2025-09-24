dbd20a237d8f251f79ad624c5c4c5625d14aa26a
jVictorHribeiro <desenvolvimento6@soliseducacional.com.br>
2025-09-24 11:28:10 -0300

    fix: issues

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -38,7 +38,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
-      // @ts-expect-error: ReactNode type mismatch due to monorepo or types conflict
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
